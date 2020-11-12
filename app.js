baa14225a7c2cb14182f4812a88f27656cf9ed5a
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-11-12 08:11:35 +0100

    Bare lige for at komme halvt i gang

---
+++ app.js
@@ -0,0 +1,17 @@
+// Hvis vi ikke er i produktionsmiljøet, skal der køres med lokale env-variable
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+// Starter Mongoose database
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+// Sætter server online
+app.listen(process.env.PORT, console.log('Server running'))
