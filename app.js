59726cbbc731c14161a7933eb69554aefb8adf4d
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-11-13 04:57:09 +0100

    Glemte at switche branch

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const Car = require('./model/Car')
 
 // Starter Mongoose database
 const mongoose = require('mongoose')
@@ -18,11 +19,25 @@ const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// const car = new Car ({
+//     brand: 'BMW',
+//     model: 'X7',
+//     licensePlate: 'fe56789',
+//     retired: false,
+//     colour: 'White',
+//     id: 1
+// })
+
+// car.save()
+// console.log(car)
+
 app.set('view engine', 'pug')
 
 // De forskellige routes
 const rootRouter = require('./routes/frontpage')
 app.use('/', rootRouter)
+
+
 
 // Sætter server online
 app.listen(process.env.PORT, console.log('Server running'))
