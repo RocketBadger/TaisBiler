b8d62c90e2c781fb998d81578ad3cf049e887e7a
Kimft3 <kimft3@gmail.com>
2020-11-16 08:52:14 +0100

    redigerBil

--- routes/cars.js
+++ routes/cars.js
@@ -4,83 +4,80 @@ const Car = require('../model/Car')
 
 // Forsiden redirecter til /biler
 router.get('/', async (request, response) => {
-    try {
-        const cars = await Car.find({})
-        response.render('cars', { cars: cars })
-    } catch (error) {
-        response.render('cars', { cars: [], errorMessage: 'Biler kunne ikke loades' })
-    }
+  try {
+    const cars = await Car.find({})
+    response.render('cars', { cars: cars })
+  } catch (error) {
+    response.render('cars', {
+      cars: [],
+      errorMessage: 'Biler kunne ikke loades',
+    })
+  }
 })
 
 // Viser siden opretBil
 router.get('/opretBil', (request, response) => {
-    try {
-        response.render('createCar')
-    } catch (error) {
-
-    }
+  try {
+    response.render('createCar')
+  } catch (error) {}
 })
 
 // Opretter en ny bil med POST
 router.post('/opretBil', async (request, response) => {
-    try {
-        let car = new Car({
-            brand: request.body.brand,
-            model: request.body.model,
-            licensePlate: request.body.licensePlate,
-            retired: request.body.retired,
-            colour: request.body.colour,
-            id: request.body.id
-        })
-        await car.save()
-        response.redirect('/biler')
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    let car = new Car({
+      brand: request.body.brand,
+      model: request.body.model,
+      licensePlate: request.body.licensePlate,
+      retired: request.body.retired,
+      colour: request.body.colour,
+      id: request.body.id,
+    })
+    await car.save()
+    response.redirect('/biler')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // Redirecter tilbage til /biler, hvis der ikke er et id
 router.get('/redigerBil', (request, response) => {
-    try {
-        response.redirect('/biler')
-    } catch (error) {
-
-    }
+  try {
+    response.redirect('/biler')
+  } catch (error) {}
 })
 
 // Finder en bil fra ID og klargør redigering
 router.get('/redigerBil/:id', async (request, response) => {
-    try {
-        const car = await Car.findById(request.params.id)
-        response.render('editCar', { car: car })
-    } catch (error) {
-
-    }
+  try {
+    const car = await Car.findById(request.params.id)
+    response.render('editCar', { car: car })
+  } catch (error) {}
 })
 
 // Opdaterer en bil fra ID med PUT
-router.put('/redigerBil', async (request, response) => {
-    console.log('put')
-    try {
-        const car = await Car.findById(request.body._id)
-        console.log(car)
-        car.brand = request.body.brand
-        car[model] = request.body.model // ændrer ellers i selve modellen, måske
-        car.licensePlate = request.body.licensePlate
-        car.retired = request.body.retired // skal ændres
-        car.colour = request.body.colour
-        car.id = request.body.id
-        console.log(car)
-        await car.save()
-        response.redirect('/biler')
-    } catch (error) {
-        console.log(error)
-    }
+router.post('/redigerBil/redigerBil', async (request, response) => {
+  console.log('put')
+  try {
+    const car = await Car.findById(request.body._id)
+    console.log(car)
+    car.brand = request.body.brand
+    car.model = request.body.model // ændrer ellers i selve modellen, måske
+    car.licensePlate = request.body.licensePlate
+    car.retired = request.body.retired // skal ændres
+    car.colour = request.body.colour
+    car.id = request.body.id
+    console.log(car)
+    await car.save()
+    response.redirect('/biler')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // Fanger alle forkert stavede versioner af sidens extension
 router.get('/:site', (request, response) => {
-    response.redirect('/')
+  response.redirect('/')
 })
 
 module.exports = router
