cfb4af3d1cba5aa8d7d09b8d541abd9c894b96a5
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-12-04 09:44:10 +0100

    login funger men autentificering ufærdig

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
 router.get('/', async (request, response) => {
     try {
         response.render('login')
@@ -11,6 +15,23 @@ router.get('/', async (request, response) => {
 })
 
 // ved korrekt login redirect til /biler
+router.post('/', async (request, response) => {
+    try {
+        if (request.body.u === process.env.USE && request.body.p === process.env.PAS || request.body.u === 'user' && request.body.p === 'pass') {
+            response.redirect('/biler')
+        } else {
+            console.log('login fucked if')
+        }
+    } catch (error) {
+        response.render('errorMessage', {
+            errorMessage: 'Der skete en fejl ved login'
+        })
+        console.log('login fucked post catch')
+        console.log(error)
+    }
+})
+
+// login => frontpage => biler ?
 
 // process.env.PAS
 // process.env.USE
