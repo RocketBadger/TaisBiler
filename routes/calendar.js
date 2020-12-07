95184639aca5389fd8cc7d7db046ff8e2989866f
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-12-07 16:58:10 +0100

    Mere tydelig errorMessage klasket på kalender

--- routes/calendar.js
+++ routes/calendar.js
@@ -3,30 +3,40 @@ const router = express.Router()
 const Car = require('../model/Car')
 
 router.get('/', async (req, res) => {
-    try {
-        const inspections = await Car.getAllInspections()
-        const repairs = await Car.getAllRepairs()
+    const inspections = await Car.getAllInspections()
+    const repairs = await Car.getAllRepairs()
+    if ((!Array.isArray(inspections) || inspections.length) && (!Array.isArray(repairs) || repairs.length)) {
+        try {
+            let date = new Date()
+            date.setDate(date.getDate() + 7)
+            let inspectionAlert = false
+            if (inspections[0].nextInspection < date) {
+                inspectionAlert = true
+            }
+            let repairAlert = false
+            console.log(date);
+            console.log(repairs[0].date);
+            console.log(repairs);
+            if (repairs[0].date < date) {
+                repairAlert = true
+            }
+            console.log(repairAlert);
 
-        let date = new Date()
-        date.setDate(date.getDate() + 7)
-        let inspectionAlert = false
-        if (inspections[0].nextInspection < date) {
-            inspectionAlert = true
+            res.render('calendar', {
+                inspections: inspections,
+                repairs: repairs,
+                inspectionAlert: inspectionAlert,
+                repairAlert: repairAlert
+            })
+        } catch (error) {
+            res.render('errorMessage', {
+                errorMessage: 'Kalender kunne ikke loades'
+            })
         }
-        let repairAlert = false
-        console.log(date);
-        console.log(repairs[0].date);
-        console.log(repairs);
-        if (repairs[0].date < date) {
-            repairAlert = true
-        }
-        console.log(repairAlert);
-
-        res.render('calendar', {
-            inspections: inspections, repairs: repairs, inspectionAlert: inspectionAlert, repairAlert: repairAlert
+    } else {
+        res.render('errorMessage', {
+            errorMessage: 'Der er ikke oprettet nogle syn/reparationer kalenderen kan vise'
         })
-    } catch (error) {
-        res.render('errorMessage', { errorMessage: 'Kalender kunne ikke loades' })
     }
 })
 
